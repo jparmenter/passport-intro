b508695c273d84ddffbcfd9688b171d048dee58c
Jeremy Parmenter <shabado9889@gmail.com>
2014-09-11 12:51:34 -0700

    Added login and made a head partial

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,23 @@
 module.exports = function(app, passport) {
 
   app.get('/', function(req, res) {
-    res.render('index.ejs');
+    res.render('pages/index.ejs');
   });
 
   app.get('/login', function(req, res) {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
+    res.render('pages/login.ejs', { message: req.flash('loginMessage') });
   });
 
   // app.post('login')
 
   app.get('/signup', function(req, res) {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
+    res.render('pages/signup.ejs', { message: req.flash('signupMessage') });
   });
 
   // app.post('/signup')
 
   app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
+    res.render('pages/profile.ejs', {
       user : req.user
     });
   });
