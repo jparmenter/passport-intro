665f69a221b3b0173636180672b93345d2f22e5f
Jeremy Parmenter <shabado9889@gmail.com>
2014-09-10 23:10:04 -0700

    initial setup of repo

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDb = require('./config/database.js');
+
+// DB
+mongoose.connect(configDb.url);
+
+//require('./config/passport')(passport);
+
+//          EXPRESS
+// Configure Express
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({ secret: 'secretmessage' }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login session
+app.use(flash()); // use connect-flash for flash messaging stored in session
+
+// routes ===============================================================
+//require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ===============================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
